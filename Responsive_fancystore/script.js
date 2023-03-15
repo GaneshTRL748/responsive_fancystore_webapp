e7c25134c24ea6ee8c61b35a483c237c5f1823f1
Ganeshkumar <123441945+GaneshTRL748@users.noreply.github.com>
2023-03-15 17:27:37 +0530

    Delete Responsive_fancystore directory

--- Responsive_fancystore/script.js
+++
@@ -1,7 +0,0 @@
-function openlightbox(img) {
-    document.getElementById("overlay").style.display = "block";
-    document.getElementById("overlay_img").src=img;
-  }
-  function removelightbox() {
-    document.getElementById("overlay").style.display = "none";
-  }
